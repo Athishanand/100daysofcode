abfb70420fe6f192ecbb3c3ce5fce9c9e2533071
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-07-25 19:54:41 +0530

    Add files via upload

---
+++ chosing colour.py
@@ -0,0 +1,26 @@
+# colour selection program
+print('choose the colour you want')
+# this function choose's the colour
+def colour():
+    if i==1:
+        x= print('violet')
+    elif i==2:
+        x= print('indigo')
+    elif i==3:
+        x= print('blue')
+    elif i==4:
+        x= print('green')
+    elif i==5:
+        x= print('yellow')
+    elif i==6:
+        x= print('orange')
+    elif i==7:
+        x= print('red')
+    else:
+        x= print('invalid output')
+        return(x)
+    
+            
+    if_name_== '__main__':
+       i=int(input('choose one number between 1 to 7:'))
+        color()
