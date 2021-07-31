938ce73548cc3442083855bed6619783e2501469
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-07-31 18:49:37 +0530

    Add files via upload

---
+++ Day8.py
@@ -0,0 +1,22 @@
+# Python program using stack implementation
+
+stack = []
+
+# append function to push  element into the stack
+
+stack.append('eat')
+stack.append('code')
+stack.append('repeat')
+stack.append('sleep')
+
+print('Initial stack')
+print(stack)
+
+# pop fucntion to pop element from stack
+
+print('\nElements poped from stack:')
+print(stack.pop())
+
+print('\nStack after elements are poped:')
+print(stack)
+
