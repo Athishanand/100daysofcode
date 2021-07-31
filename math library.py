bbd1c81b443e6cad1114281ba769247117b50d69
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-07-31 18:52:34 +0530

    Delete math library.py

--- math library.py
+++
@@ -1,9 +0,0 @@
-import math
-x = int(input("enter any number"))
-
-x==("enter a number")
-
-print("sinx = ",math.sin(x))
-print("cosx = ",math.cos(x))
-print("tanx = ",math.tan(x))
-print("degree to radians = ", math.radians(x))
