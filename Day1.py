917699ad0a426a4415b24d69228f9da3fd39a820
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-07-31 18:53:03 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,19 @@
+choice = input('enter choice(1,2,3,4,5,6,7):')
+  if i==1:
+       print ("violet")
+      elif i==2:
+       print ("indigo")
+      elif i==3:
+       print ("blue")
+      elif i==4:
+       print ("green")
+      elif i==5:
+       print ("yellow")
+      elif i==6:
+       print ("orange")
+      elif i==7:
+       print ("red")
+       
+          
+      
+      
---
+++ Day2.py
@@ -0,0 +1,9 @@
+import math
+x = int(input("enter any number"))
+
+x==("enter a number")
+
+print("sinx = ",math.sin(x))
+print("cosx = ",math.cos(x))
+print("tanx = ",math.tan(x))
+print("degree to radians = ", math.radians(x))
