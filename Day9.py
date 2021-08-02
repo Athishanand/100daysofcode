11c868c9c56aee3b4a7ba1a2a6933c9a57546a8b
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-08-02 22:50:56 +0530

    Delete Day9.py

--- Day9.py
+++
@@ -1,9 +0,0 @@
-#to creat new list
-list = [1, 1, 2, 3, 3, 3, 4, 5, 5]
-print("\nlist of numbers")
-print(list)
-
-#to find sum of the list
-list.extend(['athish', 'asm'])
-print("\nlist after addition")
-print(list)
