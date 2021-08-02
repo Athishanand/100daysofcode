1bf3cbbfb70cbf3fdaf22f30f8f38624e5d34d34
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-08-02 22:51:21 +0530

    Add files via upload

---
+++ Day9.py
@@ -0,0 +1,11 @@
+#to creat new list
+list1 = [1, 1, 2, 3, 3, 3, 4, 5, 5]
+list2 = [10, 12]
+list = list1 + list2
+print("\nlist of numbers")
+print(list)
+
+#to find sum of the list
+list.extend(['athish', 'asm'])
+print("\nlist after addition")
+print(list)
