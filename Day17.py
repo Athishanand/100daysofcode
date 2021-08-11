b53914e306538cc7768f5dfe250140a4083cbc9f
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-08-11 22:32:19 +0530

    Add files via upload

---
+++ Day17.py
@@ -0,0 +1,23 @@
+import string
+
+str1 = "athishanand" "kumar"
+str2 = 17
+str3 = "friday"
+
+print(len(str1))
+print()
+
+print(str1.upper())
+print()
+
+print(str1.format())
+print()
+
+print(str1[0:4])
+
+
+
+
+
+
+
