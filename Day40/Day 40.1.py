f035663d567aaf354e06ac6e95058374edd92bc4
Athishanand <87937454+Athishanand@users.noreply.github.com>
2021-09-03 00:29:56 +0530

    Add files via upload

---
+++ Day40/Day 40.1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import rospy
+from rmp08.srv import multiplier,multiplierResponse
+
+def multiplier_client(x,y):
+    rospy.init_node("client")
+    rospy.wait_for_service("multiplication")
+    rate=rospy.Rate(1)
+    while not rospy.is_shutdown():
+        try:
+            multiply_two_ints=rospy.ServiceProxy("multiplication",multiplier)
+            response=multiply_two_ints(x,y)
+            rospy.loginfo(response.result)
+            rate.sleep()
+        except rospy.ServiceException as e:
+            print("service call failed %s",e)
+
+
+if __name__ == "__main__":
+    multiplier_client(7,5)
---
+++ Day40/Day 40.2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+import rospy
+from rmp08.srv import multiplier,multiplierResponse
+
+def callback(request):
+    return multiplierResponse(request.a * request.b)
+
+def multiply():
+    rospy.init_node("multiplier_service")
+    service=rospy.Service("multiplication",multiplier,callback)
+    rospy.spin()
+
+if __name__ == "__main__":
+    multiply()
